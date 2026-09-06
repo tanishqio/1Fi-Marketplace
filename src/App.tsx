e2049444f83466a8e13641418d4bb263868e8696
tanishqio <bhakartanishq@gmail.com>
2026-09-06 11:40:47 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { APITester } from "./APITester";
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return 
+}
+
+export default App;
---
+++ src/components/mainBody.tsx
@@ -0,0 +1,5 @@
+export function MainBody(props){
+    return <div className="mx-auto flex min-h-screen w-full max-w-[500px] flex-1 flex-col gap-5 px-4 py-4 pb-[calc(5rem+env(safe-area-inset-bottom))]">
+        {props.children}
+    </div>
+} 
